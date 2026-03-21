571f6c1d468ea12eacd625df57b0a28a03b4d2ed
Sahan Kaushalya <mkskaushalya@gmail.com>
2026-03-21 23:58:31 +0530

    chore: add shared ESLint and TypeScript config packages
    - Add @assessment/eslint-config with TypeScript strict rules, import ordering
    - Add Next.js ESLint config extending base
    - Add @assessment/tsconfig with base, Next.js, and Worker configs
    - Enable strict mode, noUnusedLocals, noUncheckedIndexedAccess

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,26 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.next/', '.turbo/'],
+};
