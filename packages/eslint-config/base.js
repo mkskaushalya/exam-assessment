a762ee42ae3d85fa64d3edb464eb0c496c00c485
Sahan Kaushalya <mkskaushalya@gmail.com>
2026-03-23 00:39:37 +0530

    fix: resolve ESLint plugin conflict by splitting config into base and specialized versions

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -5,8 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
     'prettier',
   ],
   plugins: ['@typescript-eslint'],
@@ -14,14 +12,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/consistent-type-imports': 'error',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc' },
-      },
-    ],
     'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
   ignorePatterns: ['node_modules/', 'dist/', '.next/', '.turbo/'],
